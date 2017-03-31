78c0e7a42078fab20f239c1ccf03d729d94628bc
massjt <massjt@qq.com>
2017-03-31 21:37:25 +0800

    repair navbar active

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,9 +1,9 @@
 <header>
     <nav class="main-nav">
         <ul class="nav nav-pills nav-justified">
-            <li><a href="{{ route('blog.index') }}">Blog</a></li>
-            <li><a href="{{ route('about') }}">About</a></li>
-            <li><a href="{{ route('contact') }}">Contact</a></li>
+            <li><a class="{{ Request::is('blog/*') || Request::is('blog') ? 'main-nav-active' : '' }}" href="{{ route('blog.index') }}">Blog</a></li>
+            <li><a class="{{ Request::is('about') ? 'main-nav-active' : '' }}" href="{{ route('about') }}">About</a></li>
+            <li><a class="{{ Request::is('contact') ? 'main-nav-active' : '' }}" href="{{ route('contact') }}">Contact</a></li>
         </ul>
     </nav>
 </header>
